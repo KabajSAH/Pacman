1e0195a1ff5de9ec1c01ddd39b11ad5d6af9408c
AdrienAmoroso <adrien.amoroso@etu.univ-tours.fr>
2024-09-05 14:12:02 -0400

    [GameManager] Creation du fichier avec une base de code pour commencer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public Ghost[] ghosts;
+
+    public Pacman pacman;
+
+    public Transform pellets;
+
+    public int score { get; private set; }
+
+    public int lives { get; private set; }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    private void GhostEaten(Ghost ghost)
+    {
+        SetScore(this.score + ghost.points);
+    }
+
+    private void PacmanEaten(Pacman pacman)
+    {
+        this.pacman.gameObject.SetActive(false);
+        SetLives(this.lives - 1);
+
+        if (this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+}
--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Pacman : MonoBehaviour
 {
-
-    public GameObject test;
-
     // Start is called before the first frame update
     void Start()
     {
