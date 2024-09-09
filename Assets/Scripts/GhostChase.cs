e9ea627ab9233eb64212ac9f64e52a680b6a7cd8
Tillian <tdhume@laposte.net>
2024-09-09 13:24:01 +0200

    ghostChase

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class GhostChase : GhostBehaviour
 {
+    private void OnDisable()
+    {
+        ghost.scatter.Enable();
+    }
 
-//    private void OnDisable()
-//    {
-//        ghost.scatter.Enable();
-//    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Node node = other.GetComponent<Node>();
 
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        Node node = other.GetComponent<Node>();
+        if (node != null && enabled && !ghost.frightened.enabled)
+        {
+            Vector3 direction = Vector3.zero;
+            float minDistance = float.MaxValue;
 
-//        if (node != null && this.enabled && !this.ghost.frightened.enabled)
-//        {
-//            Vector3 direction = Vector3.zero;
-//            float minDistance = float.MaxValue;
+            foreach (Vector3 availableDirection in node.availableDirection)
+            {
+                Vector3 newPosition = transform.position + availableDirection;
+                float distance = (ghost.target.position - newPosition).sqrMagnitude;
 
-//            foreach(Vector3 availableDirection in node.availableDirection)
-//            {
-//                Vector4 newPosition = this.transform.position  +  new Vector4(availableDirection.x, 0,  availableDirection.Z,);
-//                float distance =(this.ghost.target.position - newPosition).sqrMagnitude;
+                if (distance < minDistance)
+                {
+                    direction = availableDirection;
+                    minDistance = distance;
+                }
+            }
 
-//                if (distance < minDistance) {
-//                    minDistance = distance;
-//                    direction = availableDirection;
-//                }
-
-//                this.ghost.movement.SetDirection(direction);
-
-//        }
-//    }
+            ghost.movement.SetDirection(direction);
+        }
+    }
 }
--- Assets/Scripts/GhostHome.cs
+++ Assets/Scripts/GhostHome.cs
@@ -1,6 +1,58 @@
 using UnityEngine;
+using System.Collections;
 
 public class GhostHome : GhostBehaviour
 {
+    public Transform inside;
 
+    public Transform outside;
+
+    private void OnDisable()
+    {
+        StartCoroutine(ExitTransition());
+    }
+
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator ExitTransition()
+    {
+        this.ghost.movement.SetDirection(Vector3.forward, true);
+        this.ghost.movement.rigidbody.isKinamatic = true;  
+        this.ghost.movement.enabled = false;
+
+
+        Vector3 position = this.transform.position;
+
+        float duration = 0.5f;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            Vector4 newPosition = Vector3.Lerp(position, this.inside.position, elapsed / duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            Vector4 newPosition = Vector3.Lerp(this.inside.position, this.outside.position, elapsed / duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        ghost.movement.SetDirection(new Vector3(Random.value < 0.5f ? -1f : 1f, 0f, 0f), true);
+        this.ghost.movement.SetDirection(Vector3.forward, true);
+        this.ghost.movement.rigidbody.isKenamatic = false;
+        this.ghost.mivement.enabled = true;
+
+    }
 }
