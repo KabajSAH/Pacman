603e2747e511053a01e3c63d0a29e7188daf7c24
AdrienAmoroso <adrien.amoroso@etu.univ-tours.fr>
2024-09-09 01:22:03 -0400

    Commit de mon projet

---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+    public int points = 10;
+
+    protected virtual void Eat()
+    {
+        FindObjectOfType<GameManager>().PelletEaten(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer ==  LayerMask.NameToLayer("Pacman"))
+        {
+            Eat();
+            
+        }
+    }
+}
+
